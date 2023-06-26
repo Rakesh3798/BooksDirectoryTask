1d0a96fbc359ca04cede24148629d1027e45a50b
rakesh_node <rakesh.tank@sensussoft.com>
2023-06-26 13:50:08 +0530

    commit

---
+++ controller/bookcontroller.js
@@ -0,0 +1,160 @@
+const bookModel = require("../model/bookmodel");
+const bcrypt=require("bcryptjs");
+const jwt=require("jsonwebtoken");
+
+
+const AddBook=async function (req, resp) {
+    // console.log(req);
+    const bookName = req.body.bookName;
+    const autherName = req.body.autherName;
+    const bookVersion = req.body.bookVersion;
+    const price = req.body.price;
+    const numberOfPages = req.body.numberOfPages;
+    const image = req.file.filename;
+    const password = req.body.password;
+
+    const bookExist = await bookModel.findOne({ bookName: bookName });
+
+    if (bookExist) {
+        return resp.send("Book already exists");
+    }
+    const data = await bookModel.create({
+        bookName, autherName, bookVersion, price, numberOfPages, image, password
+    });
+    //const userId = req.user.id;
+    resp.send({
+        message: "Book Updated",
+        data: data
+    });
+}
+
+const BookList= async (req, resp) => {
+    try {
+        const bookList = await bookModel.find();
+        // console.log(bookList);
+        resp.send(bookList);
+    } catch (error) {
+        console.log(error);
+        resp.status(500).send("Error retrieving book list");
+    }
+}
+
+const SearchBook=async function (req, resp) {
+    //console.log(req.query);
+    const { id, name } = req.query;
+    let query = {};
+
+    if (id) {
+        query = { _id: id };
+        //console.log(query);
+    } else if (name) {
+        query = { bookName: name };
+    }
+
+    const book = await bookModel.findOne(query);
+
+    if (!book) {
+        return resp.send("Book Not Found");
+    }
+
+    resp.send(book);
+}
+
+const UpdateBook=async function (req, resp) {
+    //console.log(req);
+    const id = req.query.id;
+    console.log(id);
+    const {
+        bookName, autherName, bookVersion, price, numberOfPages, image, password,
+    } = req.body;
+    console.log(req.body);
+
+    const bookExist = await bookModel.findOne({ _id: id });
+
+
+
+    if (!bookExist) {
+        return resp.send("Book is a Exist");
+
+    } else {
+        const updateField = (val, prev) => !val ? prev : val;
+        const updatedBook = {
+            ...bookExist,
+            bookName: updateField(bookName, bookExist.bookName),
+            autherName: updateField(autherName, bookExist.autherName),
+            bookVersion: updateField(bookVersion, bookExist.bookVersion),
+            price: updateField(price, bookExist.price),
+            numberOfPages: updateField(numberOfPages, bookExist.numberOfPages),
+            image: updateField(image, bookExist.image),
+            password: updateField(password, bookExist.password),
+        };
+        //console.log(updatedBook);
+
+        await bookModel.updateOne(
+            { _id: id },
+            {
+                $set: {
+                    bookName: updatedBook.bookName,
+                    autherName: updatedBook.autherName,
+                    bookVersion: updatedBook.bookVersion,
+                    price: updatedBook.price,
+                    numberOfPages: updatedBook.numberOfPages,
+                    image: updatedBook.image,
+                    password: updatedBook.password,
+                }
+            }
+        );
+        resp.status(200).send("Book Updated");
+    }
+}
+
+const DeleteBook= async function (req, resp) {
+    const id = req.query.id;
+    try {
+        const bookExist = await bookModel.findOne({ _id: id });
+        //console.log(bookExist);
+        if (!bookExist) {
+            return resp.send("Book Does Not Exist");
+        }
+        await bookModel.deleteOne({ _id: id });
+        console.log("Data deleted");
+        resp.send("Book Record Deleted Successfully");
+
+
+    } catch (error) {
+        console.log(error);
+        resp.status(500).send("Failed to delete book record");
+    }
+}
+
+const LoginBook= async (req, resp) => {
+    try {
+        const data = await bookModel.findOne({ bookName: req.body.bookName })
+        if (!data) {
+            resp.send("Invaild bookName and password")
+        } else {
+            const valid = await bcrypt.compare(req.body.password, data.password);
+            if (!valid) {
+                resp.send("Invaild bookName and password")
+
+            } else {
+                const token = await jwt.sign({ _id: data._id }, "thisismytokenverificatinkey");
+                console.log(token);
+                resp.send("auth-token : " + token)
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+module.exports={
+    AddBook,
+    BookList,
+    SearchBook,
+    UpdateBook,
+    DeleteBook,
+    LoginBook
+}
--- router/api.js
+++ router/api.js
@@ -1,12 +1,7 @@
 const router = require("express").Router();
-const bookModel = require("../model/bookmodel");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
-const multer = require("multer");
-const fs = require("fs");
-const path = require("path");
 const upload = require("../common");
+const {AddBook,BookList,SearchBook,UpdateBook,DeleteBook,LoginBook}=require("../controller/bookcontroller");
 
 router.post("/single", upload.single("image"), (req, res, next) => {
     //console.log(req.file);  // UPLOADED FILE DESCRIPTION RECEIVED
@@ -14,114 +9,10 @@ router.post("/single", upload.single("image"), (req, res, next) => {
     res.send("uploaded successfully");
 });
 
-router.post("/", upload.single("image"), async function (req, resp) {
-    // console.log(req);
-    const bookName = req.body.bookName;
-    const autherName = req.body.autherName;
-    const bookVersion = req.body.bookVersion;
-    const price = req.body.price;
-    const numberOfPages = req.body.numberOfPages;
-    const image = req.file.filename;
-    const password = req.body.password;
-
-    const bookExist = await bookModel.findOne({ bookName: bookName });
-
-    if (bookExist) {
-        return resp.send("Book already exists");
-    }
-    const data = await bookModel.create({
-        bookName, autherName, bookVersion, price, numberOfPages, image, password
-    });
-    //const userId = req.user.id;
-    resp.send({
-        message: "Book Updated",
-        data: data
-    });
-})
-
-
-
-router.get("/", auth, async (req, resp) => {
-    try {
-        const bookList = await bookModel.find();
-        // console.log(bookList);
-        resp.send(bookList);
-    } catch (error) {
-        console.log(error);
-        resp.status(500).send("Error retrieving book list");
-    }
-})
-
-
-router.get("/search", auth, async function (req, resp) {
-    //console.log(req.query);
-    const { id, name } = req.query;
-    let query = {};
-
-    if (id) {
-        query = { _id: id };
-        //console.log(query);
-    } else if (name) {
-        query = { bookName: name };
-    }
-
-    const book = await bookModel.findOne(query);
-
-    if (!book) {
-        return resp.send("Book Not Found");
-    }
-
-    resp.send(book);
-})
-
-
-
-router.put("/", auth, async function (req, resp) {
-    //console.log(req);
-    const id = req.query.id;
-    console.log(id);
-    const {
-        bookName, password, autherName, bookVersion, price, numberOfPages, image
-    } = req.body;
-
-    const bookExist = await bookModel.findOne({ _id: id });
-
-
-
-    if (!bookExist) {
-        return resp.send("Book is a Exist");
-
-    } else {
-        const updateField = (val, prev) => !val ? prev : val;
-        const updatedBook = {
-            ...bookExist,
-            bookName: updateField(bookName, bookExist.bookName),
-            autherName: updateField(autherName, bookExist.autherName),
-            bookVersion: updateField(bookVersion, bookExist.bookVersion),
-            price: updateField(price, bookExist.price),
-            numberOfPages: updateField(numberOfPages, bookExist.numberOfPages),
-            image: updateField(image, bookExist.image),
-            password: updateField(password, bookExist.password),
-        };
-        //console.log(updatedBook);
-
-        await bookModel.updateOne(
-            { _id: id },
-            {
-                $set: {
-                    bookName: updatedBook.bookName,
-                    autherName: updatedBook.autherName,
-                    bookVersion: updatedBook.bookVersion,
-                    price: updatedBook.price,
-                    numberOfPages: updatedBook.numberOfPages,
-                    image: updatedBook.image,
-                    password: updatedBook.password,
-                }
-            }
-        );
-        resp.status(200).send("Book Updated");
-    }
-})
+router.post("/", upload.single("image"),AddBook);
+router.get("/", auth,BookList);
+router.get("/search", auth, SearchBook);
+router.put("/", auth,UpdateBook);
 // router.put("/",auth,async function (req, resp) {
 //     const id = req.query.id;
 //     const {
@@ -273,46 +164,8 @@ router.put("/", auth, async function (req, resp) {
 //        resp.status(500).send("Internal Server Error");
 //     }
 // });
+router.delete("/",DeleteBook);
+router.post("/login",LoginBook);
 
-router.delete("/", async function (req, resp) {
-    const id = req.query.id;
-    try {
-        const bookExist = await bookModel.findOne({ _id: id });
-        //console.log(bookExist);
-        if (!bookExist) {
-            return resp.send("Book Does Not Exist");
-        }
-        await bookModel.deleteOne({ _id: id });
-        console.log("Data deleted");
-        resp.send("Book Record Deleted Successfully");
-
-
-    } catch (error) {
-        console.log(error);
-        resp.status(500).send("Failed to delete book record");
-    }
-})
-
-
-router.post("/login", async (req, resp) => {
-    try {
-        const data = await bookModel.findOne({ bookName: req.body.bookName })
-        if (!data) {
-            resp.send("Invaild bookName and password")
-        } else {
-            const valid = await bcrypt.compare(req.body.password, data.password);
-            if (!valid) {
-                resp.send("Invaild bookName and password")
-
-            } else {
-                const token = await jwt.sign({ _id: data._id }, "thisismytokenverificatinkey");
-                console.log(token);
-                resp.send("auth-token : " + token)
-            }
-        }
-    } catch (error) {
-        console.log(error);
-    }
-})
 
 module.exports = router;
